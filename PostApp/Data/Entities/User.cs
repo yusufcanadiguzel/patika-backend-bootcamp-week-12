a51a349e554a7df93cd583a101814fca84414203
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2024-09-26 01:19:15 +0300

    PostApp Completed

---
+++ PostApp/Data/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace PostApp.Data.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public List<Post> Posts { get; set; }
+    }
+}
---
+++ PostApp/Data/PatikaSecondDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PostApp.Data
+{
+    public class PatikaSecondDbContext : DbContext
+    {
+        public PatikaSecondDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
