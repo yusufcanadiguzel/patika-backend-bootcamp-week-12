98d91834f30d8b1b549e8a7e5ebb5082400d2036
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2024-09-26 01:25:34 +0300

    Update PatikaSecondDbContext.cs

--- PostApp/Data/PatikaSecondDbContext.cs
+++ PostApp/Data/PatikaSecondDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PostApp.Data.Entities;
 
 namespace PostApp.Data
 {
@@ -6,7 +7,29 @@ namespace PostApp.Data
     {
         public PatikaSecondDbContext(DbContextOptions options) : base(options)
         {
-            
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(user =>
+            {
+                user.ToTable("Users");
+                user.HasKey(u => u.Id);
+
+                user.HasMany(u => u.Posts).WithOne(p => p.User).HasForeignKey(p => p.UserId);
+            });
+
+            modelBuilder.Entity<Post>(post =>
+            {
+                post.ToTable("Posts");
+                post.HasKey(p => p.Id);
+            });
         }
     }
 }
